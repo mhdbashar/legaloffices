fe1eedb0b9df7fb260aad609fa17fdb846ae9c16
ryadshaker <ryadshaker461@gmail.com>
2020-10-11 22:19:18 +0300

    rebuild legal offices

--- DB/api_old.sql
+++ DB/api_old.sql
@@ -1,13 +1,14 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 4.7.4
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Oct 11, 2020 at 09:04 PM
--- Server version: 10.4.14-MariaDB
--- PHP Version: 7.2.33
+-- Generation Time: Jun 25, 2020 at 01:06 AM
+-- Server version: 10.1.28-MariaDB
+-- PHP Version: 5.6.32
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET AUTOCOMMIT = 0;
 START TRANSACTION;
 SET time_zone = "+00:00";
 
@@ -30,17 +31,8 @@ SET time_zone = "+00:00";
 CREATE TABLE `offic` (
   `id` int(11) NOT NULL,
   `offic_name` varchar(500) NOT NULL,
-  `token` varchar(1000) NOT NULL,
-  `office_url` varchar(200) NOT NULL,
-  `keycode` varchar(1500) NOT NULL
+  `token` varchar(1000) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
---
--- Dumping data for table `offic`
---
-
-INSERT INTO `offic` (`id`, `offic_name`, `token`, `office_url`, `keycode`) VALUES
-(107, 'شبكة بابل لخدمات القانون', 'eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJ1c2VyIjoiXHUwNjRhXHUwNjI4XHUwNjQ0IiwibmFtZSI6Ilx1MDY0YVx1MDYyOFx1MDY0NCIsIkFQSV9USU1FIjoxNjAyNDQyODQxfQ.HNoUDYwKCmbm8DoB-6PfcDB65IohyAVJJw6abPzlp9Y', 'http://localhost/office/', 'VFd0NFZWZFlSazlpVlZGNVV6SjBiazFyZEhGWFdFRTFZVmM0ZVZkV1JtNU5iR3hWVjFoS01HRllXWGxYVm1oYVkwUnNjR05WYkU5aFZ6UjVWMVpTWVZvelVuQmlha3BhV1d4d2NGUnRNVWhQVkdzMU1reFVXWEZPYlVReVMydG5Na3RxV1hBNWFXOHlXVkZuTWxsVVdtaE9iVU15UzJaYWFIUnRTVEpaV1QwPQ==');
 
 --
 -- Indexes for dumped tables
@@ -60,7 +52,7 @@ ALTER TABLE `offic`
 -- AUTO_INCREMENT for table `offic`
 --
 ALTER TABLE `offic`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=108;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
--- application/controllers/Api.php
+++ application/controllers/Api.php
@@ -1,4 +1,5 @@
 <?php
+
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 require APPPATH . 'third_party/REST_Controller.php';
@@ -6,6 +7,10 @@ require APPPATH . 'third_party/Format.php';
 
 use Restserver\Libraries\REST_Controller;
 
+header("Content-Type: application/json; charset=UTF-8");
+header("Access-Control-Allow-Methods: POST");
+header("Access-Control-Max-Age: 3600");
+header("Access-Control-Allow-Headers: Content-Type, Access-Control-Allow-Headers, Authorization, X-Requested-With");
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE");
 
@@ -13,141 +18,183 @@ class Api extends REST_Controller {
 
     public function __construct() {
         parent::__construct();
-        
+
         // Load these helper to create JWT tokens
-        $this->load->helper(['jwt', 'authorization']);    
+        $this->load->helper(['jwt', 'authorization']);
     }
 
-    public function insert_post()
-    {
-		
-	$offic_name = $this->input->post('offic_name');
-	
-  
-	 
-	 if(isset($offic_name) && !empty($offic_name)){
-		   $result=$this->db->query("select * from offic where offic_name= '". $offic_name."'")->row();
-	
-	 if($result){
- 		//$token_token=rand(1,1020120110000230);
-		//  $token = AUTHORIZATION::generateToken($token_token);
-		$token = $this->input->post('token');
-		$office_url = $this->input->post('office_url');
+    public function encode_string($office_name = '', $length = 1000) {
+
+        $key = '';
+        $k = $office_name;
+        $c = "شبكة بابل للقانون";
+
+        for ($i = 0; $i < $length; $i++) {
+            $key = base64_encode(base64_encode(base64_encode($k) . $i . $c));
+            $rr = base64_encode($key);
+        }
+
+        return $rr;
+    }
+
+    public function insert_post() {
+
+        $offic_name = $this->input->post('offic_name');
+
+        $key = $this->encode_string($offic_name);
 
 
-		$id= $result->id;
-		
-			$data = array(
-			
-				'token'		=>	$token,
-				'office_url'		=>	$office_url
 
-			);
-		 
-		 $this->db->where('id', $id)->update('offic', $data);
-            
-	 }
-	 else{
-		 
-          
-            // $token = AUTHORIZATION::generateToken(['offic_name' => $offic_name]);
-			
-			$token = $this->input->post('token');
-			$office_url = $this->input->post('office_url');
-			
-			
-			
-			$data = array(
-				'offic_name'	=>	$offic_name,
-				'token'		=>	$token,
-				'office_url'		=>	$office_url
 
-			);
-			
-			
-			 $this->db->insert('offic',$data);
 
-            
-            $status = parent::HTTP_OK;
 
-            $response = ['status' => $status, 'token' => $token];
 
-            $this->response($response, $status);
-	 }
-         }
-        else {
+        if (isset($offic_name) && !empty($offic_name)) {
+            $result = $this->db->query("select * from offic where offic_name= '" . $offic_name . "'")->row();
+
+            if ($result) {
+
+
+
+
+
+
+
+                $token = $this->input->post('token');
+                $office_url = $this->input->post('office_url');
+
+                $id = $result->id;
+
+
+
+                $data = array(
+                    'token' => $token,
+                    'office_url' => $office_url
+                );
+
+                $this->db->where('id', $id)->update('offic', $data);
+            } else {
+
+
+
+
+                $token = $this->input->post('token');
+                $office_url = $this->input->post('office_url');
+
+                $data = array(
+                    'offic_name' => $offic_name,
+                    'token' => $token,
+                    'office_url' => $office_url,
+                    'keycode' => $key
+                );
+
+
+                $this->db->insert('offic', $data);
+
+                $last_id = $this->db->insert_id();
+                $result = $this->db->query("select * from offic where id= '" . $last_id . "'")->row();
+
+                $status = parent::HTTP_OK;
+
+                $response = ['status' => $status, 'keycode' => $result->keycode];
+
+                $this->response($response, $status);
+            }
+        } else {
             $this->response(['msg' => 'where Office Name!'], parent::HTTP_NOT_FOUND);
         }
-		 
-		 
-	 
-
- 
-
     }
 
-   
-   /*  public function update_put($id)
-        {
+    function list_post() {
 
-        $input = $this->put();
-		
-        $this->db->update('offic', $input, array('id'=>$id));
-     
-        $this->response(['offic updated successfully.'], REST_Controller::HTTP_OK);
-		
-        } */
-  
-
-	function list_get()
-	{
-		
-	
-		
-		$data=$this->db->query("select id,offic_name,office_url from offic ")->result_array();
-	
-		
-		$this->response($data, REST_Controller::HTTP_OK);
-	}
-	
-	function get_token_get($offic_name)
-	{
-		
-		
-		
-		$data=$this->db->query("select office_url,token from offic where offic_name= '".$offic_name."'")->row();
-	
-		
-		$this->response($data, REST_Controller::HTTP_OK);
-	}
-	
-	
-	    function update_office_name_post() {
+  $keycode= $this->input->post('keycode');
 
 
-        $new_office_name = $this->input->post('new_office_name');
-        $old_offic_name = $this->input->post('old_offic_name');
+        if (isset($keycode) && !empty($keycode)) {
 
-        $sql = "select * from offic where offic_name = '" . $old_offic_name . "'";
-        $query = $this->db->query($sql);
 
-        $result = $query->row();
-        $id = $result->id;
+            $result = $this->db->query("select * from offic where keycode= '" . $keycode . "'")->row();
 
-        if ($id) {
+            if ($result) {
+                $data = $this->db->query("select id,offic_name,office_url from offic ")->result_array();
 
-            $data = array(
-                'offic_name' => $new_office_name
-            );
-            $this->db->where('id', $id)->update('offic', $data);
-          
+                $status = parent::HTTP_OK;
+
+               // $response = ['status' => $status, 'token' => $keycode];
+
+                $this->response($data,$status);
+            } else {
+                $this->response(['msg' => 'Invailed Token Name!'], parent::HTTP_NOT_FOUND);
+            }
+        } else {
+            $this->response(['msg' => 'Invailed Token Name!'], parent::HTTP_NOT_FOUND);
         }
     }
-	
-	
-	
-	
-	
+
+    function get_token_post() {
+
+       $keycode= $this->input->post('keycode');
+       $offic_name= $this->input->post('office_name');
+
+        if (isset($keycode) && !empty($keycode)) {
+
+            $result = $this->db->query("select * from offic where keycode= '" . $keycode . "'")->row();
+
+            if ($result) {
+
+                $data = $this->db->query("select office_url,token from offic where offic_name= '" . $offic_name . "'")->row();
+
+                $this->response($data, REST_Controller::HTTP_OK);
+            } else {
+                $this->response(['msg' => 'Invailed Token Name!'], parent::HTTP_NOT_FOUND);
+            }
+        } else {
+            $this->response(['msg' => 'Invailed Token Name!'], parent::HTTP_NOT_FOUND);
+        }
+    }
+
+    function update_office_name_post() {
+
+
+        $keycode = $this->input->post('keycode');
+        $new_office_name = $this->input->post('new_office_name');
+        $old_office_name =$this->input->post('old_office_name');
+
+
+
+
+        if (isset($keycode) && !empty($keycode)) {
+
+            $result = $this->db->query("select * from offic where keycode= '" . $keycode . "'")->row();
+
+            if ($result) {
+
+                $result = $this->db->query("select * from offic where offic_name= '" . $old_office_name . "'")->row();
+
+                $id = $result->id;
+
+
+                if ($id) {
+                    $new_key = $this->encode_string($new_office_name);
+                    $data = array(
+                        'offic_name' => $new_office_name,
+                        'keycode' => $new_key
+                    );
+                    $this->db->where('id', $id)->update('offic', $data);
+                    $result = $this->db->query("select * from offic where id= '" . $id . "'")->row();
+
+                    $status = parent::HTTP_OK;
+
+                   $this->response($result, $status);
+                 
+                }
+            } else {
+                $this->response(['msg' => 'Invailed Token Name!'], parent::HTTP_NOT_FOUND);
+            }
+        } else {
+            $this->response(['msg' => 'Invailed Token Name!'], parent::HTTP_NOT_FOUND);
+        }
+    }
 
 }
 
